50a1558b36b56592afa34172a0306df72c22e81b
Stilian Matev <stilianmatev@gmail.com>
2025-01-16 11:49:05 +0200

    added repository to category service

--- Core/Services/CategoryService.cs
+++ Core/Services/CategoryService.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Data;
+﻿using Core.Repositories;
+using DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Models;
 using System;
@@ -12,40 +13,47 @@ namespace Core.Services
     public class CategoryService : ICategoryService
     {
         private readonly ApplicationDBContext _db;
-        public CategoryService(ApplicationDBContext db)
+        private readonly IRepository<Category> _repository;
+        public CategoryService(ApplicationDBContext db, IRepository<Category> repository)
         {
+            _repository = repository;
             _db = db;
         }
         public async Task<Category> FindCategoryById(int id)
-        {
-            return await _db.Categories.FindAsync(id)
+        {//await _db.Categories.FindAsync(id)
+            return  _repository.GetById(id)
             ?? throw new KeyNotFoundException($"Category with ID {id} not found.");
         }
         public async Task<IEnumerable<Category>> GetAllCategories()
         {
-            var categories = await _db.Categories.ToListAsync();
+            var categories =  _repository.GetAll();
             return categories;
         }
         public async Task<Category> GetCategoryById(int id)
         {
-            var category = await FindCategoryById(id);
+            var category = _repository.GetById(id);
             return category;
         }
         public async Task AddCategory(Category category)
         {
-            await _db.Categories.AddAsync(category);
-            await _db.SaveChangesAsync();
+            //await _db.Categories.AddAsync(category);
+            //await _db.SaveChangesAsync();
+
+            _repository.Add(category);
         }
         public async Task EditCategory(Category category)
         {
-            _db.Categories.Update(category);
-            await _db.SaveChangesAsync();
+            //_db.Categories.Update(category);
+            //await _db.SaveChangesAsync();
+            _repository.Update(category);
         }
         public async Task DeleteCategory(int id)
         {
-            var category = await FindCategoryById(id);
-            _db.Categories.Remove(category);
-            await _db.SaveChangesAsync();
+            //var category = await FindCategoryById(id);
+            //_db.Categories.Remove(category);
+            //await _db.SaveChangesAsync();
+
+            _repository.Delete(id);
         }
 
         public  bool CategoryExists(int id)
