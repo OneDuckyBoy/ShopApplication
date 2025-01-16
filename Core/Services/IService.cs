a269112ac44c9f38e3058874b95b9b5cf9faeb40
Stilian Matev <stilianmatev@gmail.com>
2025-01-16 19:25:57 +0200

    added Service class

---
+++ Core/Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public interface IService<T> where T : class
+    {
+        T Add(T entity);
+        void Delete(int id);
+        IEnumerable<T> GetAll();
+        T GetById(int id);
+        T Update(T entity);
+        bool EntityExists(int id);
+    }
+}
---
+++ Core/Services/Service.cs
@@ -0,0 +1,50 @@
+﻿using Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Services
+{
+    public class Service<T> : IService<T> where T : class
+    {
+
+        private readonly IRepository<T> _repository;
+        public Service(IRepository<T> repository)
+        {
+            _repository = repository;
+        }
+        public T Add(T entity)
+        {
+           return _repository.Add(entity);
+        }
+
+        public bool EntityExists(int id)
+        {
+            return _repository.EntityExists(id);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            var all = _repository.GetAll();
+            
+            return all;
+        }
+
+        public T GetById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public T Update(T entity)
+        {
+            return _repository.Update(entity);
+        }
+    }
+}
